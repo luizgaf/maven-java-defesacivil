7a49c72a60758c6991d6b2fde103b7a707b6e209
Julia-Gabriela <juliagabrielagomes09@gmail.com>
2024-11-20 23:33:54 -0300

    Update

--- Defesa_Civil/src/main/java/com/view/LoginView.java
+++ Defesa_Civil/src/main/java/com/view/LoginView.java
@@ -2,53 +2,109 @@ package com.view;
 
 import com.controller.UserDAO;
 import com.model.User;
-import util.CriptografiaUtil;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginView {
+public class UserView extends JFrame {
 
-    public static void main(String[] args) {
-        UserDAO userDAO = new UserDAO();
+    private JTextField txtUsername;
+    private JPasswordField txtPassword;
+    private JButton btnCadastrar;
+    private JButton btnAutenticar;
+    private JLabel lblStatus;
+    private UserDAO userDAO;
 
-        // Opção de Login ou Cadastro
-        String[] options = {"Login", "Registrar"};
-        int choice = JOptionPane.showOptionDialog(null,
-                "Escolha uma opção",
-                "Sistema de Login",
-                JOptionPane.DEFAULT_OPTION,
-                JOptionPane.INFORMATION_MESSAGE,
-                null, options, options[0]);
+    public UserView() {
+        userDAO = new UserDAO();
 
-        if (choice == 0) {
-            // Login
-            String username = JOptionPane.showInputDialog("Digite seu usuário:");
-            String password = JOptionPane.showInputDialog("Digite sua senha:");
+        // Configurações da janela
+        setTitle("Cadastro e Autenticação de Usuário");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
-            User user = userDAO.loginUser(username, CriptografiaUtil.criptografarSenha(password));
-            if (user != null && CriptografiaUtil.verificarSenha(password, user.getPassword())) {
-                JOptionPane.showMessageDialog(null, "Bem-vindo, " + user.getUsername() + "!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Usuário ou senha inválidos!");
+        // Layout da interface
+        setLayout(new GridLayout(4, 2, 10, 10));
+
+        // Componentes
+        JLabel lblUsername = new JLabel("Nome de Usuário:");
+        txtUsername = new JTextField();
+
+        JLabel lblPassword = new JLabel("Senha:");
+        txtPassword = new JPasswordField();
+
+        btnCadastrar = new JButton("Cadastrar");
+        btnAutenticar = new JButton("Autenticar");
+        lblStatus = new JLabel("");
+
+        // Adiciona os componentes na janela
+        add(lblUsername);
+        add(txtUsername);
+        add(lblPassword);
+        add(txtPassword);
+        add(btnCadastrar);
+        add(btnAutenticar);
+        add(lblStatus);
+
+        // Ações dos botões
+        btnCadastrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cadastrarUsuario();
             }
-        } else if (choice == 1) {
+        });
 
-            String username = JOptionPane.showInputDialog("Digite seu usuário:");
-            String password = JOptionPane.showInputDialog("Digite sua senha:");
-            String role = JOptionPane.showInputDialog("Digite o papel do usuário (ex: ADMIN, USER):");
-
-            User user = new User();
-            user.setUsername(username);
-            password = CriptografiaUtil.criptografarSenha(password);
-            user.setPassword(password);
-            user.setRole(role);
-
-            if (userDAO.registerUser(user)) {
-                JOptionPane.showMessageDialog(null, "Usuário registrado com sucesso!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Erro ao registrar o usuário!");
+        btnAutenticar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                autenticarUsuario();
             }
+        });
+    }
+
+    private void cadastrarUsuario() {
+        String username = txtUsername.getText();
+        String password = new String(txtPassword.getPassword());
+
+        User usuario = new User();
+        usuario.setUsername(username);
+        usuario.setPassword(password);
+
+        boolean sucesso = userDAO.cadastrarUsuario(usuario);
+        if (sucesso) {
+            lblStatus.setText("Usuário cadastrado com sucesso.");
+            lblStatus.setForeground(Color.GREEN);
+        } else {
+            lblStatus.setText("Erro ao cadastrar o usuário.");
+            lblStatus.setForeground(Color.RED);
         }
     }
-}
 
+    private void autenticarUsuario() {
+        String username = txtUsername.getText();
+        String password = new String(txtPassword.getPassword());
+
+        User usuario = userDAO.autenticarUsuario(username, password);
+
+        if (usuario != null) {
+            lblStatus.setText("Usuário autenticado com sucesso.");
+            lblStatus.setForeground(Color.GREEN);
+        } else {
+            lblStatus.setText("Usuário ou senha inválidos.");
+            lblStatus.setForeground(Color.RED);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                UserView view = new UserView();
+                view.setVisible(true);
+            }
+        });
+    }
+}
